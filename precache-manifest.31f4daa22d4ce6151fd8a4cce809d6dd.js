274a45e3c7360e2ee139378e960a986774bc5543
bilelkh <bilel1996k@gmail.com>
2019-05-18 20:20:08 +0200

    Updates

---
+++ precache-manifest.31f4daa22d4ce6151fd8a4cce809d6dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ba62a72b9fdc19b018015b4b625d2a",
+    "url": "/contactmanger/index.html"
+  },
+  {
+    "revision": "ddd9855defffd849af45",
+    "url": "/contactmanger/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a80b6bcd5fae92ea10cf",
+    "url": "/contactmanger/static/css/main.0d411826.chunk.css"
+  },
+  {
+    "revision": "ddd9855defffd849af45",
+    "url": "/contactmanger/static/js/2.0a606ba8.chunk.js"
+  },
+  {
+    "revision": "a80b6bcd5fae92ea10cf",
+    "url": "/contactmanger/static/js/main.7c36f607.chunk.js"
+  },
+  {
+    "revision": "75553ec4f736dfb9cb31",
+    "url": "/contactmanger/static/js/runtime~main.b2d25f23.js"
+  }
+]);
